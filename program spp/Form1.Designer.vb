5222edd792c4523302ea4d298332c886da69db53
HafidzDwiRahmansyah <dwihafidz009@gmail.com>
2023-08-20 15:43:16 +0700

    Program SPP kelas 10

---
+++ program spp/Form1.Designer.vb
@@ -0,0 +1,222 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.T1 = New System.Windows.Forms.TextBox()
+        Me.T2 = New System.Windows.Forms.TextBox()
+        Me.T3 = New System.Windows.Forms.TextBox()
+        Me.T4 = New System.Windows.Forms.TextBox()
+        Me.B1 = New System.Windows.Forms.Button()
+        Me.B2 = New System.Windows.Forms.Button()
+        Me.B3 = New System.Windows.Forms.Button()
+        Me.B4 = New System.Windows.Forms.Button()
+        Me.B5 = New System.Windows.Forms.Button()
+        Me.B6 = New System.Windows.Forms.Button()
+        Me.DGV = New System.Windows.Forms.DataGridView()
+        CType(Me.DGV, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(193, 48)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(73, 19)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "ID Admin"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(193, 97)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(95, 19)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Nama Admin"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(193, 145)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(72, 19)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Password"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(193, 201)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(62, 19)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Telepon"
+        '
+        'T1
+        '
+        Me.T1.Location = New System.Drawing.Point(318, 48)
+        Me.T1.Name = "T1"
+        Me.T1.Size = New System.Drawing.Size(270, 20)
+        Me.T1.TabIndex = 5
+        '
+        'T2
+        '
+        Me.T2.Location = New System.Drawing.Point(318, 94)
+        Me.T2.Name = "T2"
+        Me.T2.Size = New System.Drawing.Size(270, 20)
+        Me.T2.TabIndex = 6
+        '
+        'T3
+        '
+        Me.T3.Location = New System.Drawing.Point(318, 138)
+        Me.T3.Name = "T3"
+        Me.T3.Size = New System.Drawing.Size(270, 20)
+        Me.T3.TabIndex = 7
+        '
+        'T4
+        '
+        Me.T4.Location = New System.Drawing.Point(318, 194)
+        Me.T4.Name = "T4"
+        Me.T4.Size = New System.Drawing.Size(270, 20)
+        Me.T4.TabIndex = 8
+        '
+        'B1
+        '
+        Me.B1.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.B1.Location = New System.Drawing.Point(56, 112)
+        Me.B1.Name = "B1"
+        Me.B1.Size = New System.Drawing.Size(75, 28)
+        Me.B1.TabIndex = 9
+        Me.B1.Text = "Tambah"
+        Me.B1.UseVisualStyleBackColor = True
+        '
+        'B2
+        '
+        Me.B2.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.B2.Location = New System.Drawing.Point(56, 150)
+        Me.B2.Name = "B2"
+        Me.B2.Size = New System.Drawing.Size(75, 28)
+        Me.B2.TabIndex = 10
+        Me.B2.Text = "Simpan"
+        Me.B2.UseVisualStyleBackColor = True
+        '
+        'B3
+        '
+        Me.B3.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.B3.Location = New System.Drawing.Point(56, 193)
+        Me.B3.Name = "B3"
+        Me.B3.Size = New System.Drawing.Size(75, 28)
+        Me.B3.TabIndex = 11
+        Me.B3.Text = "Hapus"
+        Me.B3.UseVisualStyleBackColor = True
+        '
+        'B4
+        '
+        Me.B4.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.B4.Location = New System.Drawing.Point(56, 232)
+        Me.B4.Name = "B4"
+        Me.B4.Size = New System.Drawing.Size(75, 29)
+        Me.B4.TabIndex = 12
+        Me.B4.Text = "Ubah"
+        Me.B4.UseVisualStyleBackColor = True
+        '
+        'B5
+        '
+        Me.B5.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.B5.Location = New System.Drawing.Point(56, 272)
+        Me.B5.Name = "B5"
+        Me.B5.Size = New System.Drawing.Size(75, 31)
+        Me.B5.TabIndex = 13
+        Me.B5.Text = "Batal"
+        Me.B5.UseVisualStyleBackColor = True
+        '
+        'B6
+        '
+        Me.B6.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.B6.Location = New System.Drawing.Point(56, 314)
+        Me.B6.Name = "B6"
+        Me.B6.Size = New System.Drawing.Size(75, 28)
+        Me.B6.TabIndex = 14
+        Me.B6.Text = "Tutup"
+        Me.B6.UseVisualStyleBackColor = True
+        '
+        'DGV
+        '
+        Me.DGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DGV.Location = New System.Drawing.Point(197, 262)
+        Me.DGV.Name = "DGV"
+        Me.DGV.Size = New System.Drawing.Size(344, 150)
+        Me.DGV.TabIndex = 15
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(714, 480)
+        Me.Controls.Add(Me.DGV)
+        Me.Controls.Add(Me.B6)
+        Me.Controls.Add(Me.B5)
+        Me.Controls.Add(Me.B4)
+        Me.Controls.Add(Me.B3)
+        Me.Controls.Add(Me.B2)
+        Me.Controls.Add(Me.B1)
+        Me.Controls.Add(Me.T4)
+        Me.Controls.Add(Me.T3)
+        Me.Controls.Add(Me.T2)
+        Me.Controls.Add(Me.T1)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DGV, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents T1 As System.Windows.Forms.TextBox
+    Friend WithEvents T2 As System.Windows.Forms.TextBox
+    Friend WithEvents T3 As System.Windows.Forms.TextBox
+    Friend WithEvents T4 As System.Windows.Forms.TextBox
+    Friend WithEvents B1 As System.Windows.Forms.Button
+    Friend WithEvents B2 As System.Windows.Forms.Button
+    Friend WithEvents B3 As System.Windows.Forms.Button
+    Friend WithEvents B4 As System.Windows.Forms.Button
+    Friend WithEvents B5 As System.Windows.Forms.Button
+    Friend WithEvents B6 As System.Windows.Forms.Button
+    Friend WithEvents DGV As System.Windows.Forms.DataGridView
+
+End Class
---
+++ program spp/Form1.vb
@@ -0,0 +1,68 @@
+﻿Imports System.Data.Odbc
+Public Class Form1
+    Public conn As OdbcConnection
+    Public dr As OdbcDataReader
+    Public da As OdbcDataAdapter
+    Public ds As DataSet
+    Public cmd As OdbcCommand
+    Public sql1 As String
+
+    Sub koneksi()
+        sql1 = "Driver={mysql Odbc 5.1 Driver};server=localhost;uid=root;database=db_spp_Hafidz;port=3306"
+        conn = New OdbcConnection(sql1)
+        If conn.State = ConnectionState.Closed Then
+            conn.Open()
+        End If
+    End Sub
+
+    Sub aktifkan()
+        t1.Enabled = True
+        t2.Enabled = True
+        t3.Enabled = True
+        t4.Enabled = True
+
+        B1.Enabled = True
+        B2.Enabled = True
+        B3.Enabled = True
+        B4.Enabled = True
+        B5.Enabled = True
+        B6.Enabled = True
+
+    End Sub
+
+    Sub noaktif()
+        t1.Enabled = False
+        t2.Enabled = False
+        t3.Enabled = False
+        t4.Enabled = False
+
+        B1.Enabled = False
+        B2.Enabled = False
+        B3.Enabled = False
+        B4.Enabled = False
+    End Sub
+
+    Sub tampilkanDGV()
+        koneksi()
+        da = New OdbcDataAdapter("Select * From tbladm ", conn)
+        ds = New DataSet
+        ds.Clear()
+        da.Fill(ds, "tbladm")
+        Me.DGV.DataSource = (ds.Tables("tbladm"))
+
+    End Sub
+
+    Sub kosongkan()
+        T1.Clear()
+        T2.Clear()
+        T3.Clear()
+        T4.Clear()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        koneksi()
+        tampilDGV()
+        noaktif()
+        kosongkan()
+    End Sub
+End Class
